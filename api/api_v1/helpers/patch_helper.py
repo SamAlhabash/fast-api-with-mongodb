8a1a82ea0d2bc06e1bc6e7b317d9571457897902
Sam Alhabash <Sam.alhabash@gmail.com>
2021-05-26 11:59:15 +0300

    Removed Unused Helper

--- api/api_v1/helpers/patch_helper.py
+++
@@ -1,8 +0,0 @@
-def remove_all_none_from_dict(dictionary: dict):
-    result = None
-    for key,value in dictionary.items():
-        if type(value) is dict:
-            remove_all_none_from_dict(value)
-        result.update(key, value)
-    
-    return result
